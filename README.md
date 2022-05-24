bd33052ef218c1350192d9e4c0bb45b866f18aac
abreel <64946264+abreel@users.noreply.github.com>
2022-05-24 04:47:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# des-http-processor
+ A package that sends and processes http/https requests (get/post) using axios
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import core components
+import HttpRequest from './components/HttpRequest'
+import ProcessHttpMessage from './components/ProcessHttpMessage'
+
+export default HttpRequest;
+export {
+    ProcessHttpMessage
+};
